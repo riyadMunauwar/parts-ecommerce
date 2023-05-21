c9f662e150ee286a37d3eaa2c48e63d27fff64f5
riyad <hifazakhatun1108@gmail.com>
2023-05-21 13:09:51 +0600

    payment method updated

--- resources/views/front/components/square-payment-method.blade.php
+++ resources/views/front/components/square-payment-method.blade.php
@@ -1,7 +1,7 @@
 <div>
     @if($isPaymentModeOn)
     <x-ui.edit-modal class="max-w-xl">
-        <div class="p-5 md:p-10 bg-white rounded-md">
+        <div class="p-5 md:p-10 bg-white rounded-md relative">
 
             <div class="flex justify-between items-center">
               <h5 class="text-xl font-bold dark:text-white">
@@ -112,6 +112,19 @@
                   </div>
                 @endif
             </div>
+
+            <div id="loading-spinner" style="background-color: rgba(0,0,0, .7)"  class="hidden absolute inset-0 w-full h-full z-50">
+                <divf class="w-full h-full flex items-center justify-center">
+                    <div role="status">
+                      <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                      </svg>
+                      <span class="sr-only">Loading...</span>
+                  </div>
+                </div>
+            </div>
+
             <x-ui.loading-spinner wire:loading.flex wire:target="selectedMethod, cancelPaymentMode, back" />
         </div>
     </x-ui.edit-modal>
@@ -126,33 +139,52 @@
 
   const  appId = 'sandbox-sq0idb-Xrn3FKX2QWZItCtTVZjOXA';
   const  locationId = 'LWJFEZ7KD3AZE';
+  const loadingSpinner = document.getElementById('loading-spinner');
 
-  async function initializeCard(payments) {
+  async function initializeCard(payments, payWithCardBtn) {
+    payWithCardBtn.disabled = true;
+    payWithCardBtn.innerText = 'Loading...';
+    loadingSpinner.classList.toggle('hidden');
     const card = await payments.card();
     await card.attach('#card-container'); 
+    payWithCardBtn.disabled = false;
+    payWithCardBtn.innerText = 'Pay';
+    loadingSpinner.classList.toggle('hidden');
     return card; 
   }
 
 
   async function initializeGooglePay(payments, paymentRequestOptions){
+    loadingSpinner.classList.toggle('hidden');
     const googlePay = await payments.googlePay(paymentRequestOptions);
     await googlePay.attach('#google-pay-container'); 
+    loadingSpinner.classList.toggle('hidden');
     return googlePay;
   }
 
   async function initializeApplePay(payments, paymentRequestOptions){
+    loadingSpinner.classList.toggle('hidden');
     const applePay = await payments.applePay(paymentRequestOptions);
+    loadingSpinner.classList.toggle('hidden');
     return applePay;
   }
 
-  async function initializeAchBankTransfer(payments){
+  async function initializeAchBankTransfer(payments, payWithAchBtn){
+    payWithAchBtn.disabled = true;
+    payWithAchBtn.innerText = 'Loading...';
+    loadingSpinner.classList.toggle('hidden');
     const ach = await payments.ach();
+    payWithAchBtn.disabled = false;
+    payWithAchBtn.innerText = 'Pay';
+    loadingSpinner.classList.toggle('hidden');
     return ach; 
   }
 
   async function initializeGiftCard(payments){
     const giftCard = await payments.giftCard();
+    loadingSpinner.classList.toggle('hidden');
     await giftCard.attach('#gift-card-container'); 
+    loadingSpinner.classList.toggle('hidden');
     return giftCard; 
   }
 
@@ -171,16 +203,18 @@
 
       const payments = window.Square.payments(appId, locationId);
       
+      const payWithCardBtn = document.getElementById('pay-with-card-btn');
+
       let card;
 
       try {
-        card = await initializeCard(payments);
+        card = await initializeCard(payments, payWithCardBtn);
       } catch (e) {
         console.error('Initializing Card failed', e);
+        alert(e);
         return;
       }
 
-      const payWithCardBtn = document.getElementById('pay-with-card-btn');
 
       payWithCardBtn.addEventListener('click', async function(){
 
@@ -200,6 +234,7 @@
         }catch(error){
           payWithCardBtn.disabled = false;
           payWithCardBtn.innerText = 'Pay';
+          alert(error);
         }
 
       });
@@ -214,16 +249,19 @@
    
         const payments = window.Square.payments(appId, locationId);
 
+        const payWithAchBtn = document.getElementById('pay-with-ach-btn');
+
         let ach;
 
         try {
-          ach = await initializeAchBankTransfer(payments);
+          ach = await initializeAchBankTransfer(payments, payWithAchBtn);
         } catch (e) {
           console.error('Initializing Card failed', e);
+          loadingSpinner.classList.toggle('hidden');
+          alert(e);
           return;
         }
 
-        const payWithAchBtn = document.getElementById('pay-with-ach-btn');
      
         payWithAchBtn.addEventListener('click', async function(){
           
@@ -270,6 +308,8 @@
           googlePay = await initializeGooglePay(payments, paymentRequest);
         } catch (e) {
           console.error('Initializing Card failed', e);
+          loadingSpinner.classList.toggle('hidden');
+          alert(e);
           return;
         }
 
@@ -319,6 +359,8 @@
           applePay = await initializeApplePay(payments, paymentRequest);
         } catch (e) {
           console.error('Initializing Card failed', e);
+          loadingSpinner.classList.toggle('hidden');
+          alert(e);
           return;
         }
 
@@ -360,6 +402,8 @@
       giftCard = await initializeGiftCard(payments);
     } catch (e) {
       console.error('Initializing Card failed', e);
+      loadingSpinner.classList.toggle('hidden');
+      alert(e);
       return;
     }
 
@@ -389,8 +433,6 @@
 
   })
 
-  
- 
  
 });
 
